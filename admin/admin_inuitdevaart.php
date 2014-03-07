c3a5be0afd7ca769f8dcd98231ea985db3310786
erikroos <tom.erik.roos@gmail.com>
2014-03-07 22:18:50 +0100

    Show archived out-of-service anouncements

--- admin/admin_inuitdevaart.php
+++ admin/admin_inuitdevaart.php
@@ -1,53 +1,30 @@
 <?php
-// check login
-session_start();
-if (!isset($_SESSION['authorized']) || $_SESSION['authorized'] != 'yes') {
-	header("Location: admin_login.php");
-	exit();
-}
-
-include_once("../include_globalVars.php");
-include_once("../include_helperMethods.php");
-
-$link = mysql_connect($database_host, $database_user, $database_pass);
-if (!mysql_select_db($database, $link)) {
-	echo "Fout: database niet gevonden.<br>";
-	exit();
-}
-
-setlocale(LC_TIME, 'nl_NL');
+$locationHeader = 'Vlootbeheer - In/uit de vaart';
+$backLink = '<a href="admin_vloot.php">Terug naar vlootbeheer</a>';
+include 'admin_header.php';
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" >
-<head>
-    <title><?php echo $systeemnaam; ?> - Admin - Vlootbeheer - In/uit de vaart</title>
-    <link type="text/css" href="../<?php echo $csslink; ?>" rel="stylesheet" />
-</head>
-<body>
-<div style="margin-left:10px; margin-top:10px">
-
 <?php
-
-echo "<p><strong>Welkom in de Admin-sectie van BIS</strong> [<a href='./admin_vloot.php'>Terug naar vlootbeheer</a>] [<a href='./admin_logout.php'>Uitloggen</a>]</p>";
-
+$arch = isset($_GET['arch']);
 $boot_id = $_GET['id'];
-$query = "SELECT Naam, Type from boten WHERE ID=$boot_id;";
-$result = mysql_query($query);
+$result = mysql_query('SELECT Naam, Type from boten WHERE ID = ' . $boot_id);
 $row = mysql_fetch_assoc($result);
 $name = $row['Naam'];
 
-echo '<p>Uit de Vaart-info voor: ' . $name . ' (' . $row['Type'] . ')</p>';
-echo "<p><a href=\"./admin_uitdevaart_toev.php?id=$boot_id\">Toevoegen</a></p>";
+echo '<p>' . ($arch ? 'Gearchiveerde u' : 'U') . 'it de Vaart-info voor: ' . $name . ' (' . $row['Type'] . ').';
+echo '&nbsp;Toon ' . ($arch ? '<a href="admin_inuitdevaart.php?id=' . $boot_id . '">actueel</a>' : '<a href="admin_inuitdevaart.php?id=' . $boot_id . '&arch">archief</a>') . '</p>';
+echo '<p><a href="admin_uitdevaart_toev.php?id=' . $boot_id . '">Toevoegen</a></p>';
 
 // tabel
-$query = "SELECT * from uitdevaart WHERE Verwijderd=0 AND Boot_ID=$boot_id;";
+$query = 'SELECT * from uitdevaart WHERE Verwijderd=' . ($arch ? '1' : '0') . ' AND Boot_ID=' . $boot_id . ' ORDER BY Startdatum DESC';
 $result = mysql_query($query);
 if (!$result) {
 	die("Ophalen van informatie mislukt.". mysql_error());
 }
 echo "<table class=\"basis\" border=\"1\" cellpadding=\"6\" cellspacing=\"0\" bordercolor=\"#AAB8D5\">";
-echo "<tr><th><div style=\"text-align:left\">Startdatum</div></th><th><div style=\"text-align:left\">Einddatum</div></th><th><div style=\"text-align:left\">Reden</div></th><th><div style=\"text-align:left\">Aanpassen</div></th></tr>";
+echo '<tr><th>Startdatum</th><th>Einddatum</th><th>Reden</th>';
+if (!$arch) echo '<th><div style=\"text-align:left\">Aanpassen</div></th>';
+echo '</tr>';
 $c = 0;
 while ($row = mysql_fetch_assoc($result)) {
 	$udv_id = $row['ID'];
@@ -64,16 +41,12 @@ while ($row = mysql_fetch_assoc($result)) {
 	echo "<td><div style=\"text-align:left\">$startdate_sh</div></td>";	
 	echo "<td><div style=\"text-align:left\">$enddate_sh</div></td>";
 	echo "<td><div style=\"text-align:left\">$reason</div></td>";
-	echo "<td><div><a href=\"./admin_uitdevaart_verw.php?udv_id=$udv_id&boot_id=$boot_id\">Be&euml;indigen</a></div></td>";
+	if (!$arch) echo '<td><div><a href="admin_uitdevaart_verw.php?udv_id=' . $udv_id . '&boot_id=' . $boot_id . '">Be&euml;indigen</a></div></td>';
 	echo "</tr>";
 	$c++;
 }
 echo "</table>";
-echo "<p><em>NB: Meldingen die over datum zijn, worden automatisch be&euml;indigd.</em></p>";
-
-mysql_close($link);
+echo "<p><em>NB: Meldingen met een verlopen einddatum worden automatisch gearchiveerd.</em></p>";
 ?>
 
-</div>
-</body>
-</html>
+<?php include 'admin_footer.php'; ?>
